bbcad01148a0f902c3f1f71ac612d8ad40d2645a
BTero <BTero@users.noreply.github.com>
2016-01-13 10:53:09 -0800

    Added time logic

--- js/time.js
+++ js/time.js
@@ -1,1 +1,31 @@
-location.reload()
+var time = {
+  timeFormat: config.time.timeFormat || 24,
+  dateLocation: '.date',
+  timeLocation: '.time',
+  updateInterval: 1000,
+  intervalID: null
+};
+
+/*
+* Updates the time that is shown on the screen
+*/
+
+time.updateTime = function(){
+  var _now = moment(),
+    _date = _now.format('dddd, LL');
+
+  $(this.dateLocation).html(_date);
+  $(this.timeLocation).html(_now.format(this._timeFormat+':mm[<span class="sec">]ss[</span>]'));
+}
+
+time.int = function(){
+  if(parseInt(time.timeFormat) === 12){
+    time._timeFormat = 'hh';
+  }else{
+    time._timeFormat = 'HH';
+  }
+
+  this.intervalID = setInterval(function(){
+    this.updateTime();
+  }.bind(this), 1000);
+}
