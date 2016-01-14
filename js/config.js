5d89971cf70494bed3eb094101e8821738beb41e
BTero <BTero@users.noreply.github.com>
2016-01-13 18:07:56 -0800

    Added config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+var config = {
+    lang: 'en',
+    time: {
+        timeFormat: 24
+    }/*,
+    weather: {
+        //change weather params here:
+        //units: metric or imperial
+        params: {
+            q: 'Baarn,Netherlands',
+            units: 'metric',
+            // if you want a different lang for the weather that what is set above, change it here
+            lang: 'nl',
+            APPID: 'YOUR_FREE_OPENWEATHER_API_KEY'
+        }
+    }*/
+}
