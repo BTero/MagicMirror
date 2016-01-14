656c86576ea06501a54bbf5052ea0b56e418fb1b
BTero <BTero@users.noreply.github.com>
2016-01-13 18:27:51 -0800

    Fixed typo

--- js/time.js
+++ js/time.js
@@ -18,7 +18,7 @@ time.updateTime = function(){
   $(this.timeLocation).html(_now.format(this._timeFormat+':mm[<span class="sec">]ss[</span>]'));
 };
 
-time.int = function(){
+time.init = function(){
   if(parseInt(time.timeFormat) === 12){
     time._timeFormat = 'hh';
   }else{
