3053f468a9d8da3fd2ea01c52c1ec7544549c2f9
BTero <BTero@users.noreply.github.com>
2016-01-14 21:06:01 -0800

    Added default weather configuration settings

--- js/config.js
+++ js/config.js
@@ -2,5 +2,13 @@ var config = {
     lang: 'en',
     time: {
         timeFormat: 24
+    },
+    weather: {
+      params: {
+        q: '?',                  // location
+        units: 'imperial',
+        lang: 'en',
+        APPID: 'APIID'
+      }
     }
 };
