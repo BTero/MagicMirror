c9dac5e6d6a781f500342582dc4e91ca286a60d6
BTero <BTero@users.noreply.github.com>
2016-01-14 21:03:11 -0800

    Added helper function to display info to display; Added weather initiation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+
+jQuery.fn.updateWithText = function(text,speed){
+    var temp = $('<div/>').html(text);
+
+    if ($(this).html() != temp.html()){
+      $(this).fadeOut(speed/2, function(){
+        $(this).html(text);
+        $(this).fadeIn(speed/2, function(){
+          // do nothing
+        });
+      });
+    }
+};
+
 $(document).ready(function(){
   moment.locale(config.lang);
 
   time.init();
+  weather.init();
 });
